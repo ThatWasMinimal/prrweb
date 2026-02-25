1722a614241703903392254fa1a4cbff1cce9a8f
Mal <minimal.spacing.a@gmail.com>
2026-02-25 14:33:12 +0000

    bones for the /package

---
+++ src/routes/packages/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex min-h-screen flex-col justify-center px-10 font-medium">
+    <h1 class="text-[80px] text-left">
+        packages
+    </h1>
+
+    <p class="text-sm font-thin italic text-balance">
+        packages fetching rely on HTML parsing, this mean if the aurweb layout changes or web is down then prrweb is udin din done
+    </p>
+</div>
