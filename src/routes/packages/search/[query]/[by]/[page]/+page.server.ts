25ccbdcbf758d2566583cf7c1c3718344c1d565f
Mal <minimal.spacing.a@gmail.com>
2026-02-26 09:20:28 +0000

    forgo

---
+++ src/routes/packages/search/[query]/[by]/[page]/+page.server.ts
@@ -0,0 +1,25 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params, fetch }) => {
+  const perPage = 100;
+
+  const query = params.query;
+  const by = params.by ?? "name-desc";
+  const page = Number(params.page ?? 0);
+
+  const offset = page * perPage;
+
+  const res = await fetch(
+    `https://aur.archlinux.org/rpc/v5/search/${encodeURIComponent(query)}?by=${by}`
+  );
+
+  const data = await res.json();
+
+  return {
+    results: data.results ?? [],
+    total: data.resultcount ?? 0,
+    query,
+    by,
+    page
+  };
+};
